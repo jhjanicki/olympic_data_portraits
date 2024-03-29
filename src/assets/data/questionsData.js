d93256f41de0860188f8b3434f2ae30df3820c05
Julia <>
2024-03-29 09:50:49 +0100

    added dropdown

---
+++ src/assets/data/questionsData.js
@@ -0,0 +1,5 @@
+export const questionsData = [
+{"id":"blue","question":"Tu as commencé ton sport à","answers":["0-5 ans","5-10 ans","Après 10 ans"]},
+{"id":"purple","question":"Ta force dans la pratique est plutôt basée sur la technique","answers":["Pas du tout","Un peu","Beaucoup","Absolument","À la folie","À 1000%"]},
+{"id":"pink","question":"Ta force dans la pratique est plutôt basée sur le physique","answers":["Pas du tout","Un peu","Beaucoup","Absolument","À la folie","À 1000%"]}
+]
---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { writable, derived, readable } from 'svelte/store';
+import {questionsData}  from "../assets/data/questionsData";
+
+
+export const selectedQuestion = writable("");
+export const data = readable(questionsData);
+export const selectedAnswers = writable([]);
+export const selectedAnswer = writable([]);
+export const shapeClicked = writable(false);
+export const shapeClickedOnce = writable(false);
+export const questions = derived(data,$data =>{
+    return $data.map(d=>d.question)
+});
